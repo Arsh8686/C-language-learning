46ccc2f6ee83908bde9d75f6f88918da62da3eb6
Arsh Ahmed Khan <93364088+Arsh8686@users.noreply.github.com>
2026-05-30 19:31:45 +0530

    Add files via upload

---
+++ Chapter 1/02_Input.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+    int a;
+    scanf("%d", &a);
+    printf("Your input is %d", a);
+    return 0;
+}
---
+++ Chapter 1/04_Escapesequence.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+   printf("Arsh is a student of \n Jamia Millia islamia");
+   printf("\nArsh is a student of \t Jamia Millia islamia");
+   printf("\nArsh is a student of \"Jamia Millia islamia\" ");
+    return 0;
+}
