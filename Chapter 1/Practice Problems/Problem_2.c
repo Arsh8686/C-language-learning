89396b89a1e7eee62232e3cdf5c4e9bd7123be75
Arsh Ahmed Khan <93364088+Arsh8686@users.noreply.github.com>
2026-05-31 12:58:01 +0530

    Update Problem_2.c

--- Chapter 1/Practice Problems/Problem_2.c
+++ Chapter 1/Practice Problems/Problem_2.c
@@ -1,6 +1,6 @@
 /*
 2. Calculate the area of a circle and modify the same program to calculate the 
-volume of a cylinder given its radius and height. 
+TSA of a cylinder given its radius and height. 
 */
 
 #include <stdio.h>
