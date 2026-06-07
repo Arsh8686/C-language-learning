53451bc2f81c55cb78491fd40c8932818aa4aff5
Arsh Ahmed Khan <93364088+Arsh8686@users.noreply.github.com>
2026-06-07 21:11:03 +0530

    Update Problem_4.c

--- Chapter 5/Practice Problems/Problem_4.c
+++ Chapter 5/Practice Problems/Problem_4.c
@@ -3,11 +3,8 @@
 
 int fibonacci(int n);
 int fibonacci(int n){
-    if(n == 0){
-        return 0;
-    }
-    if(n == 1){
-        return 1;
+     if(n == 1 || n == 2){
+        return n - 1;
     }
 
     return fibonacci(n-1)+fibonacci(n-2);
